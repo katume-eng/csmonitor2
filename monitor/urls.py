f3711920654e8f2f8586702026b9c4c03b030bf8
shogo <0814.sho.mura@gmail.com>
2024-08-19 09:49:32 +0900

    静的ファイルを作った.guniconeを使った

--- monitor/urls.py
+++ monitor/urls.py
@@ -4,13 +4,13 @@ from . import views
 
 urlpatterns = [
     # ex: /monitor/report/
-    path("report", views.report, name="report"),
+    path("report/", views.report, name="report"),
     # ex: /monitor/tables/
-    path("tables",views.list,name="tables"),
+    path("tables/",views.list,name="tables"),
     # ex: /monitor/aggre/
-    path("aggre",views.aggre,name="aggre"),
+    path("aggre/",views.aggre,name="aggre"),
     # ex: /monitor/display/
-    path("display",views.display,name="display"),
+    path("display/",views.display,name="display"),
     ## ex: /monitor/5/
     #path("<int:crowddata_id>/", views.detail, name="detail"),
     ## ex: /monitor/5/results/
