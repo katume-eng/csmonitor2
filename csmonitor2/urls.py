6214d269c9759dd4acdbb4541016915f844561e8
shogo <0814.sho.mura@gmail.com>
2024-08-22 08:10:48 +0900

    make report root site

--- csmonitor2/urls.py
+++ csmonitor2/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path("monitor/", include("monitor.urls")),
+    path("", include("monitor.urls")),
     path('admin/', admin.site.urls),
 ]
--- monitor/urls.py
+++ monitor/urls.py
@@ -3,18 +3,18 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # ex: /monitor/report/
-    path("report/", views.report, name="report"),
-    # ex: /monitor/tables/
+    # ex: //report/
+    path("", views.report, name="report"),
+    # ex: //tables/
     path("tables/",views.list,name="tables"),
-    # ex: /monitor/aggre/
+    # ex: //aggre/
     path("aggre/",views.aggre,name="aggre"),
-    # ex: /monitor/display/
+    # ex: //display/
     path("display/",views.display,name="display"),
-    ## ex: /monitor/5/
+    ## ex: //5/
     #path("<int:crowddata_id>/", views.detail, name="detail"),
-    ## ex: /monitor/5/results/
+    ## ex: //5/results/
     #path("<int:crowddata_id>/results/", views.results, name="results"),
-    ## ex: /monitor/5/vote/
+    ## ex: //5/vote/
     #path("<int:crowddata_id>/vote/", views.vote, name="vote"),
 ]
